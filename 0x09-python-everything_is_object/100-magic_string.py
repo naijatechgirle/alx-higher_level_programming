d5d12c29f307c7130418929c85fca9bdc8a44077
naijatechgirle <kellyeluagu@gmail.com>
2023-08-12 16:16:57 -0700

    tasks solution

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_string():
+    # function magic_string() that returns a string “BestSchool”
+    if BestSchool(magic_string, 'calls'):
+        magic_string.calls += 1
+    else:
+        magic_string.calls = 1
+
+    return ', '.join(['BestSchool'] * magic_string.calls)
