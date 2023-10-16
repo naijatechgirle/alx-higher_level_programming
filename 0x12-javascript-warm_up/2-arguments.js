b1c5eedb61108a62584044da0077086680dcb2b6
naijatechgirle <kellyeluagu@gmail.com>
2023-10-16 13:23:11 -0700

    Fixed missing character in code.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
-#!usr/bin/node
+#!/usr/bin/node
 const argsLen = process.argv.length;
-if(argsLen === 2) {
+if (argsLen === 2) {
 	console.log('No argument');
 } else if (argsLen === 3) {
 	console.log('Argument found');
