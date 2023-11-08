1e1e2a942d413057f981b0c92673686bd49cac42
Eluagu Kelechi Ruth <56093810+naijatechgirle@users.noreply.github.com>
2023-11-08 08:52:14 +0100

    Delete 0x10-python-network_0 directory
    Delecting directory

--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# displays only the status code of the response
-curl -so /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends a JSON POST request to a URL
-curl -sd "$(cat "$2")" -H 'Content-Type: application/json' "$1"
